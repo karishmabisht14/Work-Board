5779f621eee2d911bc13ec733fce4ca2179b0546
karishmabisht14 <karishmaqt14@gmail.com>
2022-07-23 10:48:38 +0530

    second commit

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { faUnlockKeyhole } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { LoginComponent } from 'src/app/main/login/login.component';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @Input()
+  content: string = '';
+  faUnlockKeyhole = faUnlockKeyhole;
+  faUser = faUser;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
